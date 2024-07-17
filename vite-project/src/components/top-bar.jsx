5e7365e9efc5b549470524ee83627f3d10678fe1
1Laura <baltrukone@gmail.com>
2024-07-17 19:42:06 +0300

    top-bar started

---
+++ vite-project/src/components/top-bar.jsx
@@ -0,0 +1,11 @@
+const TopBar = () => {
+    return (
+        <header>
+            <div className="top-bar">
+                <h1>Hello</h1>
+            </div>
+        </header>
+
+    )
+}
+export default TopBar;
