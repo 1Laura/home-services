432f5403f3ab466939fee907150d99fc6ce8776f
Laura <baltrukone@gmail.com>
2024-07-18 15:51:52 +0300

    categories mapped

--- vite-project/src/components/categories.jsx
+++ vite-project/src/components/categories.jsx
@@ -1,24 +1,40 @@
 import PropTypes from 'prop-types';
-import styles from "../pages/home.module.scss";
 import React from "react";
-import {categories} from "../pages/home.jsx";
+import styles from "./categories.module.scss";
+import cleaningImg from "../assets/categories/cleaning.png";
+import repairImg from "../assets/categories/repair.png";
+import paintingImg from "../assets/categories/painting.png";
+import shiftingImg from "../assets/categories/shifting.png";
+import plumbingImg from "../assets/categories/plumbing.png";
+import electricImg from "../assets/categories/electric.png";
 
-export function Categories({onClick, children}) {
-    return (
-        <article className={styles.categories}>
-            <div className={styles.categoryBg} onClick={onClick}>
-                <img src={categories.cleaning} alt=""/>
-                <p>{children}</p>
-            </div>
-            <div className={styles.categoryBg}>
-                <img src="" alt=""/>
-                <p>Category name</p>
-            </div>
-        </article>
-    )
-}
+export const categoriesNamesAndImg =
+    {
+        cleaning: cleaningImg,
+        repair: repairImg,
+        painting: paintingImg,
+        shifting: shiftingImg,
+        plumbing: plumbingImg,
+        electric: electricImg
+    }
+
+
+export const Categories = () => (
+    <article>
+        <div className={styles.categories}>
+            {Object.entries(categoriesNamesAndImg).map(([category, img]) => (
+                <div key={category} className={styles.categoryBg}>
+                    <img src={img} alt={category}/>
+                    <p>{category}</p>
+                </div>
+            ))}
+        </div>
+    </article>
+);
+
 
 Categories.propTypes = {
     onClick: PropTypes.func.isRequired,
     children: PropTypes.node.isRequired,
 };
+export default Categories;
--- vite-project/src/pages/home.jsx
+++ vite-project/src/pages/home.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import TopBar from "../components/top-bar.jsx";
 import styles from "./home.module.scss";
-import cleaningImg from "../assets/categories/cleaning.png";
 import {Categories} from "../components/categories.jsx";
 import Search from "../components/search.jsx";
 
-export const categories = {
-    cleaning: cleaningImg
-    // repair: "src='./assets/categories/repair.png'",
-    // painting: "src='./assets/categories/painting.png'",
-    // shifting: "src='./assets/categories/shifting.png'",
-    // plumbing: "src='./assets/categories/plumbing.png'",
-    // electric: "src='./assets/categories/electric.png'",
-
-}
 
 const Home = () => {
     return (
